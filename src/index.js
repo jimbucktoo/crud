ae34866328f1b503bac941bfb07b0672cc512adc
jimbucktoo <jamesliang.g@gmail.com>
2018-10-08 14:39:05 -0600

    constructing more efficient code.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import './style/index.css';
-import App from './components/App';
 import PostsIndex from './components/posts_index';
 import reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
