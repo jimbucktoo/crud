23e2d2c6a17368be4d487a63791fc22c5b758c18
jimbucktoo <jamesliang.g@gmail.com>
2018-10-08 14:00:00 -0600

    creation of new api key.

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=crud";
+const API_KEY = "?key=reactcrud";
 
 export function fetchPosts(){
     const request = axios.get(ROOT_URL + '/posts' + API_KEY);
